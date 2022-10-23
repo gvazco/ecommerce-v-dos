8f30b74b6de9795506f2ed87c929d53d9bddbdf4
gvazco <gustavovazco@gmail.com>
2022-10-22 22:35:21 -0500

    📦 NEW: init respository with ecommerce v2

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* ------------------------------------------------------------ */
+/*                      Depurar Errores                         */
+/* ------------------------------------------------------------ */
+
+ini_set("display_errors", 1);
+ini_set("log_errors", 1);
+ini_set("error_log", "C:/xampp/htdocs/ecommerce-v-dos/web/php_error_log");
+
+require_once "controllers/controller.template.php";
+
+$index = new TemplateController();
+$index->index();
